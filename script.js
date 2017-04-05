e7838c10055a338ffe986f00cdf1979efa253bba
Arjun Munji <arjunmunji@gmail.com>
2017-04-05 23:38:25 +0530

    choosiing colours now works

---
+++ script.js
@@ -0,0 +1,59 @@
+  const canvas = document.querySelector('#draw');
+  const ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.strokeStyle = '#00000';
+  ctx.lineWidth = 20;
+  ctx.lineJoin = 'round';
+  ctx.lineCap = 'round';
+
+  let isDrawing = false;
+  let lastX = 0;
+  let lastY = 0;
+  let hue = 0;
+  let direction = true;
+
+  function draw(e) {
+    if(!isDrawing) return;
+    // console.log(e);
+    ctx.strokeStyle = `hsl(${hue}, 100%), 50%)`;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    lastX = e.offsetX;
+    lastY = e.offsetY;
+    hue++;
+    if(hue >= 360){
+      hue = 0;
+    }
+  }
+//   if(ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+//     direction = !direction;
+//   }
+//   ctx.lineWidth++;
+
+  canvas.addEventListener('mousemove', draw);
+  canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+  });
+  canvas.addEventListener('mouseup', () => isDrawing = false);
+  canvas.addEventListener('mouseout', () => isDrawing = false);
+
+  const colours = ['black', 'white', 'red', 'blue', 'green', 'yellow', 'orange', 'purple', '#e34234', '#ffbf00', 'chartreuse', 'teal', 'violet', 'magenta' ];
+
+  var palette = document.getElementById("palette");
+  for (var i = 0; i < colours.length; i++) {
+      var colourOption = document.createElement("li");
+      console.log(colours[i]);
+      colourOption.style.backgroundColor = `${colours[i]}`;
+    //   colourOption.style.backgroundImage = "url('https://placehold.it/50x50')";
+      colourOption.classList.add("palette-colour");
+      colourOption.setAttribute('data-colour', colours[i])
+      palette.appendChild(colourOption);
+  }
+
+  $('.palette-colour').on('click', function() {
+      ctx.strokeStyle = $(this).data('colour');
+  })
